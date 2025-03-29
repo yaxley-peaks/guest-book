55d028043fb860de65ac63c309070f4cc9f0024f
yaxley-peaks <yax@yaxley.in>
2025-03-29 21:46:34 +0530

    fix build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 DOCKER_BUILDKIT=1;
 export DOCKER_BUILDKIT;
 
-docker build -o frontend/dist frontend/Dockerfile
+docker build -o frontend/dist  ./frontend/
 
 # then run nginx
 docker build -f Dockerfile -t latest . && docker run -p 3000:80 latest
