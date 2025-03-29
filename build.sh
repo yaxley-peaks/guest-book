ec96814c19c486ec00ab6d5fae2a1f6d708308c1
yaxley-peaks <yax@yaxley.in>
2025-03-29 22:13:32 +0530

    docker-compose.yml

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # first build the frontend
 
-DOCKER_BUILDKIT=1;
-export DOCKER_BUILDKIT;
-
-docker build -o frontend/dist  ./frontend/
+#DOCKER_BUILDKIT=1;
+#export DOCKER_BUILDKIT;
+#
+#docker build -o frontend/dist  ./frontend/
 
 # then run nginx
 docker build -f Dockerfile -t latest . && docker run -p 3000:80 latest
